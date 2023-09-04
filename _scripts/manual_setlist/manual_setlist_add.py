413ff08cdf72d936d655a06027a65a49bbbc92fe
lilbud <bvw20002010@gmail.com>
2023-09-04 11:59:18 -0400

    new scripts and move files

---
+++ _scripts/manual_setlist/manual_setlist_add.py
@@ -0,0 +1,48 @@
+import os, re, sqlite3
+
+from data_collection import conn, cur
+
+# to update show manually
+# add setlist
+# update on_stage
+# update all counts
+
+url = "/gig:2023-09-03-metlife-stadium-east-rutherford-nj"
+date = "2023-09-03"
+setlist_file = open("setlist.txt", "r")
+onstage_file = open("onstage.txt", "r")
+show = []
+onStage = []
+count = 1
+
+#28	1965-10-00	/gig:1965-10-00-reception-hall-monmouth-county-nj	/song:unknown	Unknown Song	Show	9	9
+#id, event_date, event_url, song_url, song_name, set_type, song_num_in_set, song_num
+for s in setlist_file.read().splitlines():
+    song_info = cur.execute(f"""SELECT song_url, song_name FROM SONGS WHERE song_name LIKE '%{s.replace("'", "''")}%'""").fetchone()
+    if len(song_info[1]) != len(s):
+        song_info = cur.execute(f"""SELECT song_url, song_name FROM SONGS WHERE song_name LIKE '{s.replace("'", "''")}'""").fetchone()
+
+    show.append([date, url, song_info[0], song_info[1], "Show", count, count])
+    count += 1
+
+cur.executemany("""INSERT OR IGNORE INTO SETLISTS VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)""", show)
+conn.commit()
+
+from data_collection import setlist_to_events
+#setlist_to_events()
+
+for o in onstage_file.read().splitlines():
+    b = cur.execute(f"""SELECT band_name FROM BANDS WHERE band_url LIKE '{o}'""").fetchone()
+    p = cur.execute(f"""SELECT person_name FROM PERSONS WHERE person_url LIKE '{o}'""").fetchone()
+
+    if b:
+        onStage.append([url, o, b[0], "Band"])
+    elif p:
+        onStage.append([url, o, p[0], "Person"])
+
+cur.executemany("""INSERT OR IGNORE INTO ON_STAGE VALUES (NULL, ?, ?, ?, ?)""", onStage)
+conn.commit()
+
+from update import update_counts
+cur.execute("vacuum;")
+#update_counts()
--- find_most_from_album.py
+++ find_most_from_album.py
@@ -10,10 +10,11 @@ f = open(f"list-{album}.txt", 'w')
 
 songs_list = []
 n_songs = []
+songs = []
 count = 0
 
-album = "The Rising"
-f = open(f"list-{album}.txt", 'w')
+album = "Tunnel Of Love"
+f = open(f"_albums/list-{album}.txt", 'w')
 
 for a in cur.execute(f"""SELECT song_name FROM ALBUMS WHERE album_name = '{album}' ORDER BY song_num ASC""").fetchall():
     songs_list.append(a[0])
@@ -22,13 +23,27 @@ id_sql = "', '".join(str(x.replace("'", "''")) for x in songs_list)
 
 for show in cur.execute(f"""SELECT DISTINCT(event_date) FROM SETLISTS WHERE song_name IN ('{id_sql}') AND event_url LIKE '%gig%' AND set_type NOT LIKE '%Soundcheck%' AND set_type NOT LIKE '%Rehearsal%'""").fetchall():
     count = 0
+    songs = []
     for song in cur.execute(f"""SELECT song_name FROM SETLISTS WHERE event_date = '{show[0]}' AND set_type NOT LIKE '%Soundcheck%' AND set_type NOT LIKE '%Rehearsal%' ORDER BY song_num""").fetchall():
         song_check = cur.execute(f"""SELECT song_name FROM ALBUMS WHERE song_name = '{song[0].replace("'", "''")}' AND album_name = '{album}'""").fetchall()
         if song_check:
             count += 1
-    
-    n_songs.append([count, show[0]])
+            if song[0] not in songs:
+                songs.append(song[0])
+
+    n_songs.append([count, show[0], songs])
 
 n_songs.sort(reverse=True)
+
+"""
+below loop is to print number of shows with a count of songs from album
+"""
+for c in [*set(i[0] for i in n_songs)]:
+    curr_count = sum(x.count(c) for x in n_songs)
+    print(f"{curr_count} show(s) have {c} song(s) from {album}")
+
 for i in n_songs:
-    f.write(f"{i[1]} has {i[0]} song(s) from {album}\n")
+    song_count = n_songs.count(count)
+    f.write(f"{i[1]} has {i[0]} song(s) from {album}:\n")
+    for s in i[2]:
+        f.write(f"\t{s}\n")
